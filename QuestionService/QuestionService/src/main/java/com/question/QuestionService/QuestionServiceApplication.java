87bc2d2a846cf58667d3e10fd8d255a353427def
vijendra12345 <vijendra.singh0682@gmail.com>
2025-12-06 17:12:58 +0530

    initialcommit

--- QuestionService/QuestionService/src/main/java/com/question/QuestionService/QuestionServiceApplication.java
+++ QuestionService/QuestionService/src/main/java/com/question/QuestionService/QuestionServiceApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuestionServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(QuestionServiceApplication.class, args);
+		System.out.println("Application Started...");
 	}
 
 }
